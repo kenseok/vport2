17ad61478d328c850eb525980f30b0857c803868
kenseok <kenseok@gmail.com>
2020-10-26 04:29:41 +0900

    billing error

--- src/App.vue
+++ src/App.vue
@@ -8,8 +8,9 @@
     <v-app-bar-nav-icon @click="drawer = !drawer" />
     <site-title :title="site.title"></site-title>
     <v-spacer/>
+    <site-sign></site-sign>
     </v-app-bar>
-    <v-navigation-drawer app v-model="drawer">
+    <v-navigation-drawer app v-model="drawer" width="400">
       <v-divider></v-divider>
       <site-menu :items="site.menu"></site-menu>
     </v-navigation-drawer>
@@ -24,9 +25,10 @@
 import SiteTitle from '@/views/site/title'
 import SiteFooter from '@/views/site/footer'
 import SiteMenu from '@/views/site/menu'
+import SiteSign from '@/views/site/sign'
 
 export default {
-  components: { SiteTitle, SiteFooter, SiteMenu },
+  components: { SiteTitle, SiteFooter, SiteMenu, SiteSign },
   name: 'App',
   data () {
     return {
