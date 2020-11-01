77c2492be7fe61dee3605afeff9af13734d8d418
kenseok <kenseok@gmail.com>
2020-11-01 16:36:07 +0900

    42

--- src/views/board/content.vue
+++ src/views/board/content.vue
@@ -4,8 +4,10 @@
       <v-toolbar color="accent" dense flat dark>
         <v-toolbar-title v-text="info.title"></v-toolbar-title>
       <v-spacer/>
-      <v-btn icon @click="write"><v-icon>mdi-pencil</v-icon></v-btn>
-      <v-btn icon @click="articleWrite"><v-icon>mdi-plus</v-icon></v-btn>
+      <template v-if="user">
+        <v-btn icon @click="write" :disabled="user.level > 0"><v-icon>mdi-pencil</v-icon></v-btn>
+        <v-btn icon @click="articleWrite" :disabled="user.level > 4"><v-icon>mdi-plus</v-icon></v-btn>
+      </template>
       </v-toolbar>
       <v-card-text v-if="info.createdAt">
         <v-alert color="info" outlined dismissible>
@@ -14,14 +16,14 @@
           <div class="text-right font-italic caption">수정일: {{info.updatedAt.toDate().toLocaleString()}}</div>
         </v-alert>
       </v-card-text>
-      <v-card-text>
-        articles
-      </v-card-text>
+      <board-article :info="info" :document="document"></board-article>
     </v-card>
   </v-container>
 </template>
 <script>
+import BoardArticle from './article/index'
 export default {
+  components: { BoardArticle },
   props: ['document'],
   data () {
     return {
@@ -37,6 +39,11 @@ export default {
   watch: {
     document () {
       this.subscribe()
+    }
+  },
+  computed: {
+    user () {
+      return this.$store.state.user
     }
   },
   created () {
