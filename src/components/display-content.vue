0376740f9634053d5a67069e41e93d238067a4ff
kenseok <kenseok@gmail.com>
2020-11-01 18:07:01 +0900

    47

---
+++ src/components/display-content.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-card>
+    <v-toolbar color="primary" dark>
+      <v-toolbar-title>
+        {{item.title}}
+      </v-toolbar-title>
+      <v-spacer/>
+      <v-btn @click="articleWrite" icon><v-icon>mdi-pencil</v-icon></v-btn>
+      <v-btn @click="$emit('close')" icon><v-icon>mdi-close</v-icon></v-btn>
+    </v-toolbar>
+    <v-card-text>
+      <viewer v-if="content" :initialValue="content"></viewer>
+      <v-container v-else>
+        <v-row justify="center" align="center">
+          <v-progress-circular indeterminate></v-progress-circular>
+        </v-row>
+      </v-container>
+    </v-card-text>
+    <v-card-actions>
+      <v-spacer/>
+      <span class="font-italic caption">
+        작성일: <display-time :time="item.createdAt"></display-time>
+      </span>
+    </v-card-actions>
+    <v-card-actions>
+      <v-spacer/>
+      <span class="font-italic caption">
+        수정일:  <display-time :time="item.updatedAt"></display-time>
+      </span>
+    </v-card-actions>
+  </v-card>
+</template>
+<script>
+import axios from 'axios'
+import DisplayTime from '@/components/display-time'
+
+export default {
+  components: { DisplayTime },
+  props: ['document', 'item'],
+  data () {
+    return {
+      content: ''
+    }
+  },
+  mounted () {
+    console.log('mounted')
+    this.fetch()
+  },
+  methods: {
+    async fetch () {
+      const r = await axios.get(this.item.url)
+      this.content = r.data
+      await this.$firebase.firestore().collection('boards').doc(this.document).collection('articles').doc(this.item.id)
+        .update({
+          readCount: this.$firebase.firestore.FieldValue.increment(1)
+        })
+    },
+    async articleWrite () {
+      this.$router.push({ path: this.$route.path + '/article-write', query: { articleId: this.item.id } })
+    }
+  }
+}
+</script>
